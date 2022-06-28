cf42fb2213dc9bad0b06483b915a02b920eb5a5b
Aprendiz Sena <kevingarcia4171@gmail.com>
2022-06-28 10:42:16 -0500

    fun contrato

--- ajax/editar_contrato.php
+++ ajax/editar_contrato.php
@@ -1,69 +1,90 @@
 <?php
 	include('is_logged.php');//Archivo verifica que el usario que intenta acceder a la URL esta logueado
-	$id_factura= $_SESSION['id_factura'];
+	$id_contrato= $_SESSION['id_contrato'];
 	/*Inicia validacion del lado del servidor*/
-	if (empty($_POST['id_cliente'])) {
-           $errors[] = "ID vacío";
-        }else if (empty($_POST['id_vendedor'])) {
-           $errors[] = "Selecciona el Usuario";
-        } else if (empty($_POST['condiciones'])){
-			$errors[] = "Selecciona forma de pago";
-		} else if ($_POST['estado_factura']==""){
-			$errors[] = "Selecciona el estado de la factura";
-		} else if (
-			!empty($_POST['id_cliente']) &&
-			!empty($_POST['id_vendedor']) &&
-			!empty($_POST['condiciones']) &&
-			$_POST['estado_factura']!="" 
-		){
+	
 		/* Connect To Database*/
 		require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
 		require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
 		// escaping, additionally removing everything that could be (html/javascript-) code
-		$id_cliente=intval($_POST['id_cliente']);
-		$id_vendedor=intval($_POST['id_vendedor']);
-		$condiciones=intval($_POST['condiciones']);
+		$num_contrato = $_POST["num_contrato"];
 
-		$estado_factura=intval($_POST['estado_factura']);
+		if ($_POST['num_poliza']==""){
+			$num_poliza="null";
+		} else {
+			$num_poliza=$_POST['num_poliza'];
+		}
+	
+		$tipo_per=$_POST["tipo_per"];
+		$id_usuario = $_POST["vendedor"];
+	
+		$fecha_inicio=$_POST["fecha_inicio"];
+		$fecha_fin=$_POST["fecha_fin"];
+	
+		$id_cliente=$_POST["id_cliente"];
+	
+		if ($_POST["clau_legal"]=="on"){
+			$clau_legal='1';
+		} else {
+			$clau_legal="0";
+		}
+	
+		if ($_POST["clau_penal"]=="on"){
+			$clau_penal="1";
+		} else {
+			$clau_penal="0";
+		}
+	
+		if ($_POST["otrosi"]==""){
+			$otrosi='null';
+		} else {
+			$otrosi="'".$_POST["otrosi"]."'";
+		}
+	
+		if ($_POST["duracion"]==""){
+			$duracion = '1';
+		} else {
+			$duracion = $_POST["duracion"];
+		}
+	
+		$archivo = $_FILES['archivo'];
+		$fecha_act=date("Y-m-d");
+		$observacion = $_POST["observaciones"];
+		$calculado = str_replace(',','',$_POST["calculado"]);
+
 		
-		$sql="UPDATE facturas SET id_cliente='".$id_cliente."', id_vendedor='".$id_vendedor."', condiciones='".$condiciones."', estado_factura='".$estado_factura."' WHERE id_factura='".$id_factura."'";
+		if($archivo['name']){
+			$name = "(".$id_contrato.")".$archivo['name'];
+			$ruta = $archivo['tmp_name'];
+			$destino = "../archivos/" . $name;
+			$sql="UPDATE contrato SET fecha_inicio='".$fecha_inicio."', fecha_final='".$fecha_fin."', fecha_act='".$fecha_act."',tipo_per='".$tipo_per."',id_cliente='".$id_cliente."',duracion='".$duracion."',numcontrato='".$num_contrato."', numpoliza='".$num_poliza."',clausulagal='".$clau_legal."', clausulapenal='".$clau_penal."', descripcion='".$observacion."', valor='".$calculado."',id_contrato_rel=".$otrosi.", id_usuario='".$id_usuario."', ruta='".$destino."', archivo='".$name."' WHERE id_contrato='".$id_contrato."'";
+		} else {
+			$sql="UPDATE contrato SET fecha_inicio='".$fecha_inicio."', fecha_final='".$fecha_fin."', fecha_act='".$fecha_act."',tipo_per='".$tipo_per."',id_cliente='".$id_cliente."',duracion='".$duracion."',numcontrato='".$num_contrato."', numpoliza='".$num_poliza."',clausulagal='".$clau_legal."', clausulapenal='".$clau_penal."', descripcion='".$observacion."', valor='".$calculado."',id_contrato_rel=".$otrosi.", id_usuario='".$id_usuario."' WHERE id_contrato='".$id_contrato."'";
+		}
+
+	
+		
 		$query_update = mysqli_query($con,$sql);
 			if ($query_update){
-				$messages[] = "Factura ha sido actualizada satisfactoriamente.";
+				$messages[0] = "Factura ha sido actualizada satisfactoriamente.";
 			} else{
-				$errors []= "Lo siento algo ha salido mal intenta nuevamente.".mysqli_error($con);
+				$errors [0]= "Lo siento algo ha salido mal intenta nuevamente.".mysqli_error($con);
 			}
-		} else {
-			$errors []= "Error desconocido.";
-		}
 		
-		if (isset($errors)){
-			
-			?>
+	if (isset($errors)){
+		echo json_encode('
 			<div class="alert alert-danger" role="alert">
-				<button type="button" class="close" data-dismiss="alert">&times;</button>
-					<strong>Error!</strong> 
-					<?php
-						foreach ($errors as $error) {
-								echo $error;
-							}
-						?>
-			</div>
-			<?php
-			}
-			if (isset($messages)){
-				
-				?>
-				<div class="alert alert-success" role="alert">
-						<button type="button" class="close" data-dismiss="alert">&times;</button>
-						<strong>¡Bien hecho!</strong>
-						<?php
-							foreach ($messages as $message) {
-									echo $message;
-								}
-							?>
-				</div>
-				<?php
-			}
-
+			<button type="button" class="close" data-dismiss="alert">&times;</button>
+			<strong>Error!</strong>'.$errors[0].
+			'</div>'
+		);
+	}	
+	if (isset($messages)){				
+		echo json_encode('
+			<div class="alert alert-success" role="alert">
+			<button type="button" class="close" data-dismiss="alert">&times;</button>
+			<strong>¡Bien hecho!</strong>'.$message[0].
+			'</div>'
+	);
+	}
 ?>
