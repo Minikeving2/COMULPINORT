f389a7e381052826f858e8a209e74fe18ed44dea
Aprendiz Sena <kevingarcia4171@gmail.com>
2022-06-28 10:57:19 -0500

    act files contrato

--- ajax/editar_contrato.php
+++ ajax/editar_contrato.php
@@ -52,11 +52,17 @@
 		$observacion = $_POST["observaciones"];
 		$calculado = str_replace(',','',$_POST["calculado"]);
 
-		
+
 		if($archivo['name']){
+			$sql =  "SELECT ruta FROM contrato WHERE id_contrato='$id_contrato'";
+			$datos = mysqli_fetch_array(mysqli_query($con,$sql));
+			//el unlink me elimina el fichero segun la ubicacion y su nombre
+			unlink($datos[0]);
+
 			$name = "(".$id_contrato.")".$archivo['name'];
 			$ruta = $archivo['tmp_name'];
 			$destino = "../archivos/" . $name;
+			copy($ruta, $destino);
 			$sql="UPDATE contrato SET fecha_inicio='".$fecha_inicio."', fecha_final='".$fecha_fin."', fecha_act='".$fecha_act."',tipo_per='".$tipo_per."',id_cliente='".$id_cliente."',duracion='".$duracion."',numcontrato='".$num_contrato."', numpoliza='".$num_poliza."',clausulagal='".$clau_legal."', clausulapenal='".$clau_penal."', descripcion='".$observacion."', valor='".$calculado."',id_contrato_rel=".$otrosi.", id_usuario='".$id_usuario."', ruta='".$destino."', archivo='".$name."' WHERE id_contrato='".$id_contrato."'";
 		} else {
 			$sql="UPDATE contrato SET fecha_inicio='".$fecha_inicio."', fecha_final='".$fecha_fin."', fecha_act='".$fecha_act."',tipo_per='".$tipo_per."',id_cliente='".$id_cliente."',duracion='".$duracion."',numcontrato='".$num_contrato."', numpoliza='".$num_poliza."',clausulagal='".$clau_legal."', clausulapenal='".$clau_penal."', descripcion='".$observacion."', valor='".$calculado."',id_contrato_rel=".$otrosi.", id_usuario='".$id_usuario."' WHERE id_contrato='".$id_contrato."'";
